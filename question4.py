5d9f8808b516e6d351dd7e131798d53492f76ed8
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-11-15 11:29:43 -0500

    Practicum 3 (Answer for Question 4)

--- question4.py
+++ question4.py
@@ -1,3 +1,15 @@
 def longest_words(word_list):
-    """  Your solution goes here, see instructions for details """
+    """
+    This function loops through words in a list, adds to the dictionary the longest word for any first character
+    """
+    longest_word_dict = dict()
+    
+    for word in word_list:
+        if word[0] not in longest_word_dict:
+            longest_word_dict[word[0]] = word
+        else:
+            if len(word) > len(longest_word_dict[word[0]]):
+                longest_word_dict[word[0]] = word
+    
+    return longest_word_dict
 
