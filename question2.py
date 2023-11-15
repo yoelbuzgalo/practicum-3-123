9847249293eaab3474d4841c932eb347a3eadde4
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-11-15 11:22:17 -0500

    Practicum 3, Question 3 committed/answered and Question 2 still WIP

--- question2.py
+++ question2.py
@@ -1,8 +1,35 @@
 def fish_sort (a_list):
-    """  Your solution goes here, see instructions for details """
+    """
+    This function finds and removes the smallest value in an unsorted list and adds it to a sorted list.
+    """
+    unsorted_list = [element for element in a_list] # Copies every element in a_list using list comprehension, this is to prevent pass by reference modification
+    sorted_list = [] # Initialize empty list
+
+    while len(unsorted_list) > 0:
+        # Find the maximum value in a list
+        max_value = 0
+        for element in unsorted_list:
+            if element > max_value:
+                max_value = element
+            
+            # Find the mininum value
+        min_value = max_value # Start with the highest value
+        for element in unsorted_list:
+            if element < min_value:
+                min_value = element
         
+        for i in range(0,len(unsorted_list)):
+            if unsorted_list[i] == min_value:
+                sorted_list.append(unsorted_list.pop(i)) # Pop the same smallest minimum value from the list and append it to the sorted list
+
+    return sorted_list
+
+
+
 def main ():
     '''Ad Hoc tests you write should go in here'''
+    some_list = [5,3,10,2,1]
+    fish_sort(some_list)
 
 if __name__ == "__main__":
     main ()
--- question3.py
+++ question3.py
@@ -1,5 +1,11 @@
 def unique_elements(a_sequence):
-    """  Your solution goes here, see instructions for details """
+    """
+    This function generates and returns a list of unique elements in a given sequence
+    """
+
+    unique_set = set(a_sequence) # Initialize an unique set, passing every item in a sequence
+
+    return [set_item for set_item in unique_set] # Return a list of unique items
 
 # For manual testing purposes, you may change as needed
 def main():
